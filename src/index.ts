09d55171ec2f11b374206190e9135c9b682050a2
Stephen J Smith <ssmith323@gmail.com>
2021-08-14 07:28:11 -0500

    move around the map

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import { distplayHeader } from './graphics/graphics';
 import Location from './map/location';
 
 distplayHeader();
+console.log('Welcome to Jorgen Island. It is a world where anything is');
+console.log('possible! You can battle other people and find the love of');
+console.log('your life. To get started lets create a character.');
 const location = new Location();
 let keepPlaying = true;
 while (keepPlaying) {
@@ -16,11 +19,12 @@ while (keepPlaying) {
     case 'SOUTH':
     case 'EAST':
     case 'WEST':
-      console.log('LOCATION', location.move(input));
+      location.move(input);
+      location.event();
       break;
     default:
       console.log('I dont understand');
   }
 }
-console.log('Exit');
+console.log('Thank you for playing! Goodbye!');
 process.exit(0);
--- src/map/location.ts
+++ src/map/location.ts
@@ -1,9 +1,11 @@
+import { Places, worldMap } from './world';
+
 export default class Location {
   private x: number;
   private y: number;
   constructor() {
-    this.x = 0;
-    this.y = 0;
+    this.x = 5;
+    this.y = 5;
   }
   move(direction: CardinalDirections): CurrentLocation {
     switch (direction) {
@@ -21,6 +23,32 @@ export default class Location {
         break;
     }
     return [this.x, this.y];
+  }
+
+  event(): void {
+    let terrian: Places;
+    try {
+      terrian = worldMap[this.x][this.y];
+    } catch (e) {
+      terrian = Places.DEEP_OCEAN;
+    }
+
+    switch (terrian) {
+      case Places.WATER:
+        console.log('Careful now. You are in the ocean. You could get lost.');
+        break;
+      case Places.COAST_LINE:
+        console.log('You are enjoying walking with the tide hitting your feet');
+        break;
+      case Places.BEACH:
+        console.log('The sand on the beach is hot!');
+        break;
+      default:
+        console.log(
+          'You better find land soon or you could be swimming for a while.',
+        );
+    }
+    console.log(`[${this.x}, ${this.y}]`);
   }
 }
 export type CardinalDirections = 'NORTH' | 'SOUTH' | 'EAST' | 'WEST';
